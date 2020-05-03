4cce7f3a3f227f4db7afdc728b3eb245e1cf48a1
Jeff Hallmark <jhallmark1007@gmail.com>
2020-05-03 14:31:50 -0400

    Update Make files and Make directories

--- scripts/requireproj.sh
+++ scripts/requireproj.sh
@@ -8,14 +8,14 @@ else
 NAME="-passed bad arg-"
 NAMING="-passed bad arg-"
 fi
-@printf "\n$NAMING tejas/tejascaptcha will completely delete the current version of the package.\n"
-@printf "Upon successful completion of the $NAME run 'cd .' to resync the new current directory contents\n"
-@printf "with the terminals shell.\n\n"
-@echo "Proceed with  $NAMING tejas/tejascaptcha?"
-@echo ""
-@select YN in "Yes" "No"
-@do
-@case $YN in
+printf "\n$NAMING tejas/tejascaptcha will completely delete the current version of the package.\n"
+printf "Upon successful completion of the $NAME run 'cd .' to resync the new current directory contents\n"
+printf "with the terminals shell.\n\n"
+echo "Proceed with  $NAMING tejas/tejascaptcha?"
+echo ""
+select YN in "Yes" "No"
+do
+case $YN in
 Yes)  cd ../../ && composer require tejas/tejascaptcha && cd .
 break
 ;;
@@ -24,11 +24,11 @@ break
 ;;
 *) printf "Invalid entry. Enter '1' for Yes or '2' for No\n1) Yes\n2) No\n"
 ;;
-@esac
-@done
-@echo ""
+esac
+done
+echo ""
 if [ Xrubbish != X$YN ] ; then
-@printf "$NAMING tejas/tejascaptcha has deleted and recreated the current project directory.\n"
-@printf "Upon successful completion of the $NAME run 'cd .' to resync the new current directory contents\n"
-@printf "with the terminals shell.\n\n"
+printf "$NAMING tejas/tejascaptcha has deleted and recreated the current project directory.\n"
+printf "Upon successful completion of the $NAME run 'cd .' to resync the new current directory contents\n"
+printf "with the terminals shell.\n\n"
 fi
--- scripts/testdir.sh
+++ scripts/testdir.sh
@@ -1,12 +1,12 @@
-@echo ""
+echo ""
 TPATH=../../storage/app/audio
 if [ -d $TPATH ] ; then
-@printf "Make sure the correct owner and permissions are set on the storage/app/audio directory.\n"
-@printf "Currently they are: \t" && ls -las ../../storage/app | grep audio
-@printf "\nThe owner should be set to the web servers user, e.g. www-data for Apache2\n"
+printf "Make sure the correct owner and permissions are set on the storage/app/audio directory.\n"
+printf "Currently they are: \t" && ls -las ../../storage/app | grep audio
+printf "\nThe owner should be set to the web servers user, e.g. www-data for Apache2\n"
 else
-@printf "The "$TPATH" directory does not exist.\n"
-@printf "Make sure the correct owner and permissions are set on the storage/app directory.\n"
-@printf "Currently they are: \t" && ls -las ../../storage | grep app
-@printf "After the owner and permissions are verified run 'make install'."
+printf "The "$TPATH" directory does not exist.\n"
+printf "Make sure the correct owner and permissions are set on the storage/app directory.\n"
+printf "Currently they are: \t" && ls -las ../../storage | grep app
+printf "After the owner and permissions are verified run 'make install'."
 fi
