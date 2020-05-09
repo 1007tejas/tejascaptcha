3067544c1dbefdd859a2db8a2e9c9c6ab40d264d
Jeff Hallmark <jhallmark1007@gmail.com>
2020-05-08 20:37:39 -0400

    Update Make files

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-MAKELOC="../tejas/tejascaptcha/"
+ORIGMAKEFILELOC="../tejas/tejascaptcha"
+NEWMAKEFILELOC="../../tejascaptcha"
 
-if [ -f $(MAKELOC)"Makefile" ] ; then
-cp $(MAKELOC)Makefile ../../../tejas_Makefile;
-printf $(MAKELOC)"Makefile"
-printf "\n\tThe tejas_Makefile has been copied to the root of your project.
-\tYou may run 'make -f tejas_Makefile [update, install, remove, test or version]'.\n";
-printf "\n\tIf the tejas_Makefile is the only Makefile in the projects root directory
-\tyou may rename it to 'Makefile'. This reduces the 'make' programs invocation to:
-\t'make: [update, install, remove, test or version]'\n\n"
+if [ -f $ORIGMAKEFILELOC"/Makefile" ] ; then
+mkdir $NEWMAKEFILELOC
+cp $ORIGMAKEFILELOC"/Makefile" $NEWMAKEFILELOC"/";
+printf "\n\tThe tejascaptcha Makefile has been copied to tejascaptcha directory
+\tin the root of your project.\n
+\tYou may run 'tejascaptcha/make [update, install, remove, test or version]'.\n";
 fi
