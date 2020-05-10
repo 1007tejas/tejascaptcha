cfc6e9d2e2885c17027a55daf8c147acacc2a22d
Jeff Hallmark <jhallmark1007@gmail.com>
2020-05-10 19:50:04 -0400

    Update Make files

--- scripts/filesdirs.sh
+++ scripts/filesdirs.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-printf "\nAttempting to create the storage/app/audio directory\n"
 RESULT=1
 if [ -d "../../storage" ] ; then
+printf "\nAttempting to create the storage/app/audio directory\n"
 if [ ! -d "../../storage/app/audio" ] ; then
-RESULT="$(mkdir -p ../../../storage/app/audio)"
-fi
+RESULT=`mkdir -p ../../storage/app/audio`
 else
-RESULT=0
+RESULT=
+fi
 fi
 if [ "X" = "X"$RESULT ] ; then
 printf "Success!\n"
+printf "Make sure the correct owner and permissions are set on the storage/app/audio directory.\n"
+printf "\nCurrent permissions and owner are: \t" && ls -las ../../storage/app | grep audio
+printf "The owner should be set to the web servers user, e.g. www-data for Apache2\n\n"
 else
 printf "Could not create the audio directory.\n\n"
 printf "Make sure the correct owner and permissions are set on the storage/app directory.\n"
-printf "Currently they are: \t" && ls -las ../../storage | grep app
-printf "\nThe owner should be set to the web servers user, e.g. www-data for Apache2\n"
+printf "\nCurrent permissions and owner are: \t" && ls -las ../../storage | grep app
+printf "The owner should be set to the user that created the Laravel app\n\n"
 fi
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ORIGMAKEFILELOC="../tejas/tejascaptcha"
-NEWMAKEFILELOC="../../tejascaptcha"
+ORIGMAKEFILELOC="../"
+NEWMAKEFILELOC="../../../../tejascaptcha"
 
-if [ -f $ORIGMAKEFILELOC"/Makefile" ] ; then
+if [ -f $ORIGMAKEFILELOC"Makefile" ] ; then
 [ -d NEWMAKEFILELOC ] || mkdir $NEWMAKEFILELOC
-\cp -r -f $ORIGMAKEFILELOC"/Makefile" $NEWMAKEFILELOC"/";
+\mv $ORIGMAKEFILELOC"/Makefile" $NEWMAKEFILELOC"/";
 printf "\n\tThe tejascaptcha Makefile has been copied to tejascaptcha directory
 \tin the root of your project.\n
 \tyou may run 'cd tejascaptcha && make [update, install, remove, test or version]'.\n";
