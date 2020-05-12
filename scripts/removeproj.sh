d8b430d67ef7adedeaa727476fee583a77174f3f
Jeff Hallmark <jhallmark1007@gmail.com>
2020-05-12 11:51:57 -0400

    Update Make files

--- scripts/removeproj.sh
+++ scripts/removeproj.sh
@@ -8,14 +8,10 @@ case $YN in
 Yes)  cd ../../ && composer remove tejas/tejascaptcha && composer clearcache && \rm tejascaptcha
 break
 ;;
-No) YN="rubbish"
+No) exit 0
 break
 ;;
 *) printf "Invalid entry. Enter '1' for Yes or '2' for No\n1) Yes\n2) No\n"
 ;;
 esac
 done
-echo ""
-if [ Xrubbish != X$YN ] ; then
-exit 1
-fi
--- scripts/requireproj.sh
+++ scripts/requireproj.sh
@@ -18,14 +18,10 @@ case $YN in
 Yes)  cd ../../ && composer remove tejas/tejascaptcha && composer clearcache && composer require tejas/tejascaptcha
 break
 ;;
-No) YN="rubbish"
+No) exit 0
 break
 ;;
 *) printf "Invalid entry. Enter '1' for Yes or '2' for No\n1) Yes\n2) No\n"
 ;;
 esac
 done
-echo ""
-if [ Xrubbish != X$YN ] ; then
-exit 1
-fi
