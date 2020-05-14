0f2577a88bc1fb3267b8fc74f39898e425bfbe1f
Jeff Hallmark <jhallmark1007@gmail.com>
2020-05-14 09:30:14 -0400

    Update Make files

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ORIGMAKEFILELOC="../"
-NEWMAKEFILELOC="../../../tejascaptcha"
+NEWMAKEFILELOC="../../../../tejascaptcha"
 
 if [ -f $ORIGMAKEFILELOC"Makefile" ] ; then
-[ -d NEWMAKEFILELOC ] || mkdir $NEWMAKEFILELOC
-\mv $ORIGMAKEFILELOC"/Makefile" $NEWMAKEFILELOC"/";
+[ -d NEWMAKEFILELOC ] || mkdir -p $NEWMAKEFILELOC
+\cp $ORIGMAKEFILELOC"/Makefile" $NEWMAKEFILELOC"/";
 printf "\n\tThe tejascaptcha Makefile has been copied to tejascaptcha directory
 \tin the root of your project.\n
 \tyou may run 'cd tejascaptcha && make [update, install, remove, test or version]'.\n";
